651cb7072d3202e6ca2d2b9da4220cbbc91a9c1a
Abid ali <khanali3229>
2024-05-06 22:21:15 +0500

    first i give js to mini-circle give some animation

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+
+function circleMouseFollower() {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#mini-circle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+  });
+}
+circleMouseFollower();
